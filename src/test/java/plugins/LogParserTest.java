7531197b25603efd25792e407a2b84d6c300cfc2
larca <michael.91@gmx.de>
2017-04-20 15:26:27 +0200

    post-push

--- src/test/java/plugins/LogParserTest.java
+++ src/test/java/plugins/LogParserTest.java
@@ -19,14 +19,18 @@ import java.util.regex.Pattern;
 @WithPlugins("log-parser")
 public class LogParserTest extends AbstractJUnitTest {
 
+    private LogParserProjectConfig config;
+
     @Before
     public void globalConfig() {
         jenkins.configure();
-        LogParserProjectConfig config = new LogParserProjectConfig(jenkins.getConfigPage());
-        config.addParserConfig("description", "path");
+        config = new LogParserProjectConfig(jenkins.getConfigPage());
         jenkins.save();
     }
 
+    /**
+     * Sample Test
+     */
     @Test
     public void testing(){
         FreeStyleJob j = jenkins.jobs.create(FreeStyleJob.class, "simple-job");
@@ -47,7 +51,24 @@ public class LogParserTest extends AbstractJUnitTest {
 
     }
 
+    /**
+     * Adds a post-build-step to the job which prints out the content of the specified file. 
+     *
+     * @param job The job where the post-build-step is added.
+     * @param str The name of the file which shall be printed out.
+     */
     private void catToConsole(final Job job, final String str) {
         job.addShellStep("cat " + str);
+    }
+
+    /**
+     * Adds a new rule to the existing config.
+     * @param description The description of the new rule.
+     * @param pathToFile The path to the rule file.
+     */
+    private void addLogParserRule(final String description, final String pathToFile){
+        jenkins.configure();
+        config.addParserConfig(description, pathToFile);
+        jenkins.save();
     }
 }
